78f214f7c688ed7822fe2714ef6a98b297680a78
Duleynis <mitya.04tr@mail.ru>
2026-08-11 22:12:07 +0500

    Revert "Убрал деление" This reverts commit ef94606de7ca97c56f3bce36f6591c9002356575.

--- Preparing.cpp
+++ Preparing.cpp
@@ -3,19 +3,12 @@
 
 using namespace std;
 
-// Сложение чисел
 float Sum(float firstNumber, float secondNumber) {
 	return firstNumber + secondNumber;
 }
 
-// Умножение чисел
 float Multiplication(float firstNumber, float secondNumber) {
 	return firstNumber * secondNumber;
-}
-
-// Деление чисел
-float Division(float firstNumber, float SecondNumber) {
-	return firstNumber / SecondNumber;
 }
 
 int main()
@@ -26,6 +19,5 @@ int main()
 	cin >> first >> second;
 
 	cout << "Сумма чисел: " << Sum(first, second) << "\n";
-	cout << "Произведение чисел: " << Multiplication(first, second) << "\n";
-	cout << "Частное чисел " << Division(first, second);
+	cout << "Произведение чисел: " << Multiplication(first, second);
 }
