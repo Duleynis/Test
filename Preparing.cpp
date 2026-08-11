4c9f586080ff54b2c7c3761461e67c939c52c30a
Duleynis <mitya.04tr@mail.ru>
2026-08-11 17:24:09 +0500

    Добавил умножение

---
+++ Preparing.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+
+float Sum(float firstNumber, float secondNumber) {
+	return firstNumber + secondNumber;
+}
+
+float Multiplication(float firstNumber, float secondNumber) {
+	return firstNumber * secondNumber;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	float first, second;
+	cin >> first >> second;
+
+	cout << "Сумма чисел: " << Sum(first, second) << "\n";
+	cout << "Произведение чисел: " << Multiplication(first, second);
+}
